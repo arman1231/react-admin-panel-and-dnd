72ff5057a03bdeed9ab7ebd270996d12ca6c4a14
Arman <arman1231@yandex.ru>
2022-10-19 17:00:44 +0300

    predeploy changes

--- src/components/FeaturedInfo/FeaturedInfo.jsx
+++ src/components/FeaturedInfo/FeaturedInfo.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import './featuredInfo.scss'
+import React from "react";
+import "./featuredInfo.scss";
+import { Link } from "react-router-dom";
 
 export default function FeaturedInfo() {
   return (
-    <div className='featured'>Это главный компонент</div>
-  )
+    <div className="featured">
+      <h1>Это главный компонент</h1>
+      <p>Вы можете перейти в <Link to='/planner'>Планировщик</Link> или посмотреть <Link to='/users'>список пользователей</Link></p>
+    </div>
+  );
 }
--- src/pages/Planner/Planner.jsx
+++ src/pages/Planner/Planner.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./planner.scss";
 import { useDrop } from "react-dnd";
 import Picture from "./Picture";
+import axios from "axios";
 
 export default function Planner() {
   const imageList = [
@@ -33,6 +34,37 @@ export default function Planner() {
   const handleDelete = () => {
     setBoard([])
   }
+
+  const exportPlan = (data) => {
+    const fileData = JSON.stringify(data);
+    const blob = new Blob([fileData], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = "plan.json";
+    link.href = url;
+    link.click();
+  }
+
+  const [file, setFile] = useState()
+  function handleChange(event) {
+    setFile(event.target.files[0])
+  }
+  function handleUpload(event) {
+    event.preventDefault()
+    const url = 'http://localhost:3000/uploadFile';
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', file.name);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios.post(url, formData, config).then((response) => {
+      console.log(response.data);
+    });
+
+  }
   return (
     <div className="planner">
       <div className="plannerWrap">
@@ -43,8 +75,15 @@ export default function Planner() {
             ))}
           </div>
           <div className="pictures">
-            <button className="plannerSaveBtn userAddButton">Сохранить</button>
+            <button className="plannerSaveBtn userAddButton" onClick={() => exportPlan(board)}>Сохранить</button>
             <button className="plannerDelBtn userAddButton" onClick={handleDelete}>Очистить</button>
+          </div>
+          <div className="upload pictures">
+                       
+          <form onSubmit={handleUpload}>
+            <input type="file" name="file" onChange={handleChange} />
+            <button className="plannerImportBtn userAddButton" type="submit">Импортировать</button>
+            </form>
           </div>
         </div>
         <div className="board" ref={drop}>
